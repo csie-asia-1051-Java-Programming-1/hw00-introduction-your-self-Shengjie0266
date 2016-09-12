323634ca39a7909417dc012042de690bb951a363
Shengjie0266 <105021040@live.asia.edu.tw>
2016-09-12 15:50:20 +0800

    hw00 homework submit
    Introduction

---
+++ hw00-1/src/Shengjie.java
@@ -0,0 +1,13 @@
+
+public class Shengjie {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("亞洲大學資工系數位內容組一年級洪聖傑");
+        
+	}
+
+}
